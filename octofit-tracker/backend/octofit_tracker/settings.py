b77bd212d420e130fadbb5b8d503e948e5cd11d0
berfersa <33541302+berfersa@users.noreply.github.com>
2025-12-18 15:57:25 +0000

    test: verify /api/teams/ endpoint and update summary

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-m1e%45maiq-ac^+aun*)20r#dg-9b@^^_m-ana^&*zaj3mxg#3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME', None)
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME and isinstance(CODESPACE_NAME, str):
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
+ALLOWED_HOSTS.append('*')
 
 
 # Application definition
